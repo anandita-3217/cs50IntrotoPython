5a1d8a357bdd5cf938aed78a71e27de13f1a754f
Anandita Dakshayani <dakshugarimella@gmail.com>
2024-09-03 22:48:27 +0530

    updated watch.py

--- regexp/watch/watch.py
+++ regexp/watch/watch.py
@@ -7,11 +7,14 @@ def main():
 
 
 def parse(s):
-    ...
-
-
-...
-
+   pattern_in = r'src="https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+)"' 
+   match = re.search(pattern_in,s)
+   if match:
+       pattern_out = match.group(1)
+       return f"https://youtu.be/{pattern_out}"
+   return None
+#Cant believ ive been rickrolled in 2024!
+#Well played
 
 if __name__ == "__main__":
     main()
